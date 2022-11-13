420fd9d11bbec7bd291660043847ea09d2a8ac7f
alexander <alex.shklyarenko22@gmail.com>
2022-11-13 11:53:00 +0200

    v1.0.1 commit 2

---
+++ src/GeoServiceProvider.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace AingShIp\GeoIpServicePackage;
+
+
+use Alexander\GeoServiceInterface\GeoServiceInterface;
+use Illuminate\Support\ServiceProvider;
+
+
+class GeoServiceProvider extends ServiceProvider
+{
+    public function register()
+    {
+        $this->app->singleton(GeoServiceInterface::class, function () {
+          return new IpApiGeoService();
+        });
+    }
+}
---
+++ src/IpApiGeoService.php
@@ -0,0 +1,65 @@
+<?php
+
+namespace AingShIp\GeoIpServicePackage;
+
+use Alexander\GeoServiceInterface\GeoServiceInterface;
+use GeoIp2\Database\Reader;
+use http\Client\Request;
+use Illuminate\Support\Facades\Http;
+
+
+class IpApiGeoService implements GeoServiceInterface
+{
+    /**
+     * @Package AingSh\Alexander\PackageHillelGeoTest v1.0.0
+     */
+    /**
+     * @param string $ip
+     * @return void
+     */
+    public function parse(string $ip): void
+    {
+        $response = Http::get($this->getUrl($ip));
+
+        $this->_data = $response->json();
+    }
+
+
+    /**
+     * @return string|null
+     */
+    public function getIsoCode(): ?string
+    {
+        return $this->_data['continentCode'] ?? null;
+
+    }
+
+    /**
+     * @return string|null
+     */
+    public function getCountry(): ?string
+    {
+
+        return $this->_data['countryCode'] ?? null;
+
+    }
+
+    /**
+     * @param string $ip
+     * @return string
+     */
+    protected function getUrl(string $ip): string
+    {
+        $url = 'http://ip-api.com/json/' . $ip;
+        $parameters = http_build_query([
+            'fields' => 'continentCode,countryCode,query',
+
+        ]);
+        return $url . '?' . $parameters;
+    }
+
+
+}
+
+
+
